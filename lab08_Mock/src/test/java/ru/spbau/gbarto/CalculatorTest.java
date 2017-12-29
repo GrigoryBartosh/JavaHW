27182c56269dc29547896b42a7dfff5f1ff72518
Grigory Bartosh <grishabartosh@yandex.ru>
2017-12-29 17:59:10 +0300

    lab #08 junit5 fix

--- lab08_Mock/src/test/java/ru/spbau/gbarto/CalculatorTest.java
+++ lab08_Mock/src/test/java/ru/spbau/gbarto/CalculatorTest.java
@@ -1,12 +1,11 @@
 package ru.spbau.gbarto;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 class CalculatorTest {
